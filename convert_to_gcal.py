b4dbab6da36ab64714ef37d5ee9a979c19cae4c1
rogevand <rvand516@gmail.com>
2020-09-03 20:09:40 -0400

    udpate

--- convert_to_gcal.py
+++ convert_to_gcal.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 dates = ["Jan", "January", "Feb", "February", "Mar", "March", "Apr", "April", "May", "Jun", "June", "Jul", "July", "Aug", "August", "Sep", "Sept", "September", "Oct", "October", "Nov", "November", "Dec", "December"]
 
-df = pd.read_csv('output.csv')
+df = pd.read_csv('output.csv') # set a conditional to determine of this is a docx(future update)
 #print(df)
 
 event_array = []
 
 for index, row in df.iterrows():
-    if row.equals(dates):
+    print(row)
+    if row.index == 2:
         event_array.append(row)
 
 print(str(event_array))
-
